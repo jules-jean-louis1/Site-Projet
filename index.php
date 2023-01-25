447033276c683ba157fb2d6d13dc2400fcdd5f4d
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-01-25 12:01:14 +0100

    2eme commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script defer src="script.js"></script>
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/8b26d30613.js" crossorigin="anonymous"></script>
+    <!-- Tailwindccs -->
+    <script src="https://cdn.tailwindcss.com"></script>    
+</head>
+<body>
+    <header>
+        <div class="flex w-full bg-red-100 sticky ">
+            <div class="flex px-6">
+                <nav>
+                    <ul class="flex py-3">
+                        <li class="nav-link">
+                            <a href="#home-page" class="mx-2 ">
+                                Accueil
+                            </a>
+                        </li>
+                        <li><a href="#project-made">Projets</a></li>
+                        <li><a href="#about-me">A propos</a></li>
+                        <li><a href="#contact-me">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main>
+        <article class="home-page"></article>
+        <article class="project-made"></article>
+        <article class="about-me"></article>
+        <article class="contact-me"></article>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
