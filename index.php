c302b33474ab4bf0e558fe0a7bc0195e5151dda5
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-02-06 14:44:40 +0100

    animate with js on scroll

--- index.php
+++ index.php
@@ -6,36 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio</title>
     <link rel="stylesheet" href="css/style.css">
-    <script defer src="script.js"></script>
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/8b26d30613.js" crossorigin="anonymous"></script>
-    <!-- Tailwindccs -->
-    <script src="https://cdn.tailwindcss.com"></script>    
+    <script defer src="script.js"></script> 
 </head>
 <body>
-    <header>
-        <div class="flex w-full bg-red-100 sticky ">
-            <div class="flex px-6">
-                <nav>
-                    <ul class="flex py-3">
-                        <li class="nav-link">
-                            <a href="#home-page" class="mx-2 ">
-                                Accueil
-                            </a>
-                        </li>
-                        <li><a href="#project-made">Projets</a></li>
-                        <li><a href="#about-me">A propos</a></li>
-                        <li><a href="#contact-me">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>
+
     <main>
-        <article class="home-page"></article>
-        <article class="project-made"></article>
-        <article class="about-me"></article>
-        <article class="contact-me"></article>
+        <section class="hidden">
+            <h1>Hello</h1>
+            <p>Salut</p>
+        </section>
+        <section class="hidden">
+            <h1>Hello</h1>
+            <p>Salut</p>
+        </section>
+        <section class="hidden">
+            <h1>Hello</h1>
+            <p>Salut</p>
+        </section>
+        <section class="hidden">
+            <h1>Hello</h1>
+            <p>Salut</p>
+        </section>
     </main>
     <footer>
 
