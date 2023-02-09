3769c0eef5a6b317081abfbb8a7f14b5c30b53d0
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-02-09 23:16:45 +0100

    change

--- index.php
+++ index.php
@@ -12,15 +12,15 @@
     <script defer src="script.js"></script> 
 </head>
 <body>
-    <header>
-        <nav>
-            <ul class="flex justify-between">
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="projets.php">Projets</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+<header class="fixed top-2 left-0 right-0 z-10 mx-2">
+    <nav class="bg-[#202124fc] rounded-full py-2">
+        <ul class="flex justify-between items-center px-3">
+            <li><a href="index.php" class="text-white"><img src="img/jjl-3d-v2.svg" alt="" class="w-9"></a></li>
+            <li><a href="projets.php" class="text-white">Projets</a></li>
+            <li><a href="contact.php" class="text-white">Contact</a></li>
+        </ul>
+    </nav>
+</header>
     <main>
         <article>
             <section class="w-full flex flex-col items-center">
@@ -28,7 +28,7 @@
                     <h1>Projets</h1>
                 </div>
                 <div id="containerProject" class="flex flex-col justify-center items-center mx-5 space-y-5">
-                    <div id="project1" class="bg-[#181A1D] hover:bg-[#202124] rounded-lg">
+                    <div id="project1" class="bg-[#181A1D] hover:bg-[#202124] rounded-lg ease-in duration-300">
                         <div id="imgContainer" class="">
                             <img src="projet/oclock.jpg" alt="Oclock" class="object-cover object-center rounded-t-lg">
                         </div>
@@ -44,9 +44,15 @@
                                 </span>
                             </p>
                         </div>
-                        <div id="linkProject">
-                            <a href="#">Voir le projet</a>
-                            <a href="#">Voir le code</a>
+                        <div id="linkProject" class="flex justify-center mb-2 space-x-2 text-[#707173]">
+                            <a href="#" class="bg-[#202124] px-2 rounded hover:text-[#2137fc]">
+                                <i class="fa-solid fa-globe"></i>
+                                Projet
+                            </a>
+                            <a href="#" class="bg-[#202124] px-2 rounded hover:text-[#2137fc]">
+                                <i class="fa-brands fa-github"></i>
+                                Github
+                            </a>
                         </div>
                     </div>
                     <div id="project1" class="bg-[#181A1D] hover:bg-[#202124]">
